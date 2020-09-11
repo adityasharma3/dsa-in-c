31229cb09c8422e5205db7ca61b9c588cc574eab
Aditya Sharma <aditya.sharma9301@gmail.com>
2020-09-11 22:30:23 +0530

    updated singly-linked-lists

---
+++ linked-lists/singly-linked-list/inserting-at-start.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+void insert(struct Node *head , int new_data) {
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+	new_node->data = new_data;
+	new_node->next = *head;
+
+	*head = new_node;
+}
+
+void main() {
+	struct Node *head = NULL;
+	struct Node *second = NULL;
+	struct Node *third = NULL;
+
+	head = (struct Node*)malloc(sizeof(struct Node));
+	second = (struct Node*)malloc(sizeof(struct Node));
+	third = (struct Node*)malloc(sizeof(struct Node));
+
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	insert(head ,head->data);
+}
